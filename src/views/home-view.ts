a32ef543b62db355845c1bdb2dc039d43b442efa
Xavier <xmtrinidad@gmail.com>
2021-10-09 21:48:14 -0700

    first drop example

--- src/views/home-view.ts
+++ src/views/home-view.ts
@@ -3,7 +3,7 @@ import { SHARED_STYLES } from '../styles/SHARED_STYLES';
 
 export class HomeView extends LitElement {
 
-  @property({type: String}) exampleProp = 'This is an example prop';
+  @property({ type: Object }) interact;
 
   static styles = [
     SHARED_STYLES,
@@ -11,16 +11,56 @@ export class HomeView extends LitElement {
       :host {
         display: block;
       }
+
+      .draggable-thing {
+        width: 100px;
+        height: 100px;
+        background-color: rebeccapurple;
+      }
     `
   ];
 
   render() {
     return html`
       <div class="home-view">
-        <h1>Home View works</h1>
-        <p>${this.exampleProp}</p>
+        <div class="draggable-thing">
+          <span>This is a draggable thing</span>
+        </div>
       </div>
     `;
+  }
+
+  constructor() {
+    super();
+    // Recast Window object
+    const w = <any>window;
+
+    // Set interact instance
+    this.interact = w.interact;
+  }
+
+  firstUpdated() {
+    this.moveThing();
+  }
+
+  moveThing() {
+    const position = { x: 0, y: 0 }
+
+    this.interact('.draggable-thing').draggable({
+      listeners: {
+        start(event) {
+          console.log(event.type, event.target)
+        },
+        move(event) {
+          position.x += event.dx
+          position.y += event.dy
+
+          event.target.style.transform =
+            `translate(${position.x}px, ${position.y}px)`
+        },
+      }
+    });
+
   }
 }
 
